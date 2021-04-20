d8870cc9bf8100b4adfffdf221ee6befe91edd6f
FRACE <frace@inmoods.com>
2021-04-20 20:21:00 +0800

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,55 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <button @click="hello">axios请求</button>
+    <button @click="imgRequest">图片请求</button>
+    <button @click="jsonpRequest">jsonp请求</button>
+    <button @click="corsRequest">cors请求</button>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  methods: {
+    hello() {
+      axios.get("http://127.0.0.1:3000/", {
+        params: {
+          ID: 12345,
+        },
+      });
+    },
+    imgRequest() {
+      const img = new Image();
+      img.src = "http://localhost:3000?id=www";
+    },
+    jsonpRequest() {
+      const script = document.createElement("script");
+      const funName = `jsonp${Math.random()
+        .toString(16)
+        .substr(2)}`;
+      console.log(funName);
+      script.src = `http://127.0.0.1:3000/jsonp?funName=${funName}`;
+      window[funName] = (data) => {
+        console.log(data);
+        document.body.removeChild(script);
+        window[funName] = null;
+      };
+      document.body.appendChild(script);
+    },
+    corsRequest() {
+      axios({
+        url: "http://localhost:3000/cors",
+        method: "post",
+        withCredentials: true,
+      }).then((res) => {
+        console.log(res.data);
+      });
+    },
+  },
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
